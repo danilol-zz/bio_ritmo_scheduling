5b3379fe8a1c3255a27cb40abae839fa8c01ac14
Danilo Lima <danilo.lima@victus.com.br>
2015-03-16 17:14:29 -0300

    gems

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,14 @@
 module ApplicationHelper
-  def check_situation(room, date, hour)
-    scheduling_date = DateTime.new(date.year, date.month, date.day, hour)
+  def check_situation(room, date_param, hour)
+    date = date_param.to_date
+    scheduling_date = DateTime.new(date.year, date.month, date.day, hour.to_i)
     scheduling      = Scheduling.find_by(room_id: room.id, time: scheduling_date)
-    scheduling ? "Reservado para outro" : "Disponível"
+    scheduling      ? "Reservado para #{scheduling.user.name}" : "Disponível"
   end
 
-  def days_of_week(day = Date.today)
+  def days_of_week
     days = []
-    start = day.to_date.beginning_of_week
+    start = Date.today.beginning_of_week
     0.upto(6) { |n| days << (start + n.day).strftime("%d/%m/%Y") }
 
     days
--- app/models/scheduling.rb
+++ app/models/scheduling.rb
@@ -1,4 +1,6 @@
 class Scheduling < ActiveRecord::Base
   validates_presence_of :room_id
   validates_presence_of :time
+
+  belongs_to :user
 end
